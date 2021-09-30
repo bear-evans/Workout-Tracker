e8b4ed95f6a2f41d4f5266105d83a5f2f35d5223
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-09-29 20:16:21 -0400

    Added get and update routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,46 @@
 const router = require("express").Router();
-const Workout = require("../../models/Workout");
+const { response } = require("express");
+const db = require("../models");
 
 // Workout Routes
-router.get("/api/workouts", (req, res) => {
-  res.json("Work in progress!");
+// ---------------------------------------------------------
+// Get all workouts
+router.get("/api/workouts", async (req, res) => {
+  try {
+    let results = await db.Workout.find({});
+
+    results.forEach((workout) => {
+      let total = 0;
+      workout.exercises.forEach((exercise) => {
+        total = total + exercise.duration;
+      });
+
+      workout.totalDuration = total;
+    });
+
+    res.json(results);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// ---------------------------------------------------------
+// update a workout
+router.put("api/workouts/:id", async (req, res) => {
+  try {
+    let results = await db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    );
+
+    res.json(results);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
